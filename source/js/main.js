73af288c4ad23f2e97fc28eb3645c8b1d9c278e2
Shurafads <wypafads@gmail.com>
2022-08-19 17:48:31 +0300

    добавляет js на навигацию

---
+++ source/js/main.js
@@ -0,0 +1,19 @@
+let button = document.querySelector('.navigation__toggle');
+let list = document.querySelector('.navigation__list');
+
+
+function toggleClass (element, className) {
+  element.classList.toggle(className);
+}
+
+function removeClass (element, className) {
+  element.classList.remove(className);
+}
+
+//removeClass(list, "navigation__list--opened");
+//removeClass(button, "navigation__toggle--opened");
+
+button.addEventListener("click", function () {
+  toggleClass(list, "navigation__list--opened");
+  toggleClass(button, "navigation__toggle--opened");
+});
