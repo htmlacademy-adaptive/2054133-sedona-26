f7b5f922632019c91b0a0d1cba29254146ecc40d
Keks <keks@htmlacademy.ru>
2022-09-13 19:56:50 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/script.js
+++ 9/js/script.js
@@ -1,22 +1,4 @@
-let button = document.querySelector('.navigation__toggle');
-let list = document.querySelector('.navigation__list');
-let mainNav = document.querySelector('.navigation');
 
-mainNav.classList.remove('navigation--nojs');
-button.classList.remove('navigation__toggle--nojs');
-
-function toggleClass (element, className) {
-  element.classList.toggle(className);
-}
-
-function removeClass (element, className) {
-  element.classList.remove(className);
-}
-
-removeClass(list, "navigation__list--opened");
-removeClass(button, "navigation__toggle--opened");
-
-button.addEventListener("click", function () {
-  toggleClass(list, "navigation__list--opened");
-  toggleClass(button, "navigation__toggle--opened");
-});
+let button=document.querySelector('.navigation__toggle');let list=document.querySelector('.navigation__list');let mainNav=document.querySelector('.navigation');mainNav.classList.remove('navigation--nojs');button.classList.remove('navigation__toggle--nojs');function toggleClass(element,className){element.classList.toggle(className);}
+function removeClass(element,className){element.classList.remove(className);}
+removeClass(list,"navigation__list--opened");removeClass(button,"navigation__toggle--opened");button.addEventListener("click",function(){toggleClass(list,"navigation__list--opened");toggleClass(button,"navigation__toggle--opened");});
